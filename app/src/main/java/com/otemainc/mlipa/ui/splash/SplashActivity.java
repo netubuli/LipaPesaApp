5ca8256b45fb988510f309391897612b8180834e
TheOnlySmartBoy <o2jose43@gmail.com>
2019-10-10 17:33:05 +0300

    Completed writing the splash screen backend code

--- app/src/main/java/com/otemainc/mlipa/ui/splash/SplashActivity.java
+++ app/src/main/java/com/otemainc/mlipa/ui/splash/SplashActivity.java
@@ -1,7 +1,5 @@
 package com.otemainc.mlipa.ui.splash;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -9,41 +7,60 @@ import android.widget.TextView;
 
 import com.otemainc.mlipa.MainActivity;
 import com.otemainc.mlipa.R;
+import com.otemainc.mlipa.ui.auth.SignUpActivity;
+import com.otemainc.mlipa.util.SharedPref;
 
-/***
- *
- */
-public class SplashActivity extends AppCompatActivity {
-    ProgressBar progress;
-    TextView loaderLabel;
+public class SplashActivity extends Activity {
+    private ProgressBar mProgress;
+    private TextView loaderLabel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Show the splash screen
         setContentView(R.layout.activity_splash);
-        progress = findViewById(R.id.progressBar);
+        mProgress = findViewById(R.id.progressBar);
         loaderLabel = findViewById(R.id.progressMessage);
-        progress.setIndeterminate(true);
-        String[] launcher_message = {"Initializing system","Starting engine","opening payment gateway","initializing stack","Initializing security",
-        "Loading pools","More security initialization","Starting system","Loading app","Updating local database"};
-        int i = 0;
-        while (i < launcher_message.length) {
-            for (int j =0; j<=100; j++) {
-                if(j==100 && 1==9){
-                    Intent loadMain = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(loadMain);
-                    finish();
-                }else {
-                    loaderLabel.setText(new StringBuilder().append(launcher_message[i]).append(" ").append(j).append(" %").toString());
-                    i++;
-                }
-
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
             }
+        }).start();
+    }
+    String[] launcher_message = {"Initializing\tsystem","Starting\tengine","opening\tpayment\tgateway",
+            "initializing\tstack","Initializing\tsecurity","Loading\tpools","More\tsecurity\tinitialization",
+            "Starting\tsystem","Loading\tapp","Updating\tlocal\tdatabase"};
 
+    private void doWork() {
+        for (int progress=0; progress<101; progress+=10) {
+            try {
+                Thread.sleep(1000);
+                mProgress.setProgress(progress);
+                for(int i = 0; i<launcher_message.length; i++) {
+                    loaderLabel.setText(launcher_message[i]+" "+progress+" %");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                           }
         }
-
-
-
     }
 
+    private void startApp() {
+//Check if the user has been authenticated
+        if (SharedPref.getInstance().getString("is_registered").equalsIgnoreCase("")) {
+            //User is not yet logged in
+            // show the login activity
+            Intent login = new Intent(SplashActivity.this, SignUpActivity.class);
+            startActivity(login);
+        }else {
+            //User has logged in
+            //Show the HomeActivity
+            Intent home = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(home);
+        }
+        finish();
+    }
 }
