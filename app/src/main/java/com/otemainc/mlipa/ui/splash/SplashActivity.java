585ca4843520a12010daa1e126a709af24b79009
TheOnlySmartBoy <o2jose43@gmail.com>
2019-10-10 13:18:30 +0300

    Begun writing the splash screen ui code

---
+++ app/src/main/java/com/otemainc/mlipa/ui/splash/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.otemainc.mlipa.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.otemainc.mlipa.R;
+
+/***
+ *
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+
+}
