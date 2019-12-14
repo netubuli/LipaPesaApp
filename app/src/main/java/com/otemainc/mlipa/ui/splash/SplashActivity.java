0329d21c0fa22b3891ca49f9eecbd640ce1c53b4
TheOnlySmartBoy <o2jose43@gmail.com>
2019-12-14 19:15:50 +0300

    Updated dashboard design

--- app/src/main/java/com/otemainc/mlipa/ui/splash/SplashActivity.java
+++ app/src/main/java/com/otemainc/mlipa/ui/splash/SplashActivity.java
@@ -2,26 +2,41 @@ package com.otemainc.mlipa.ui.splash;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
+import com.beardedhen.androidbootstrap.TypefaceProvider;
 import com.otemainc.mlipa.ui.MainActivity;
 import com.otemainc.mlipa.R;
 import com.otemainc.mlipa.ui.auth.LoginActivity;
 import com.otemainc.mlipa.util.helper.SessionManager;
 
 public class SplashActivity extends Activity {
-    private ProgressBar mProgress;
+    private com.beardedhen.androidbootstrap.BootstrapProgressBar mProgress;
     private TextView loaderLabel;
     private SessionManager session;
+    ViewFlipper flipper, textFlipper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Show the splash screen
         setContentView(R.layout.activity_splash);
+        TypefaceProvider.registerDefaultIconSets();
+        flipper = findViewById(R.id.Flipper);
+        textFlipper = findViewById(R.id.titles);
         mProgress = findViewById(R.id.progressBar);
         loaderLabel = findViewById(R.id.progressMessage);
+        int[] images = {R.drawable.slide, R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4};
+        int[] titles = {R.string.slide, R.string.slide1, R.string.slide2, R.string.slide3, R.string.slide4};
+        for(int image:images){
+            flipperImages(image);
+        }
+        for(int title:titles) {
+            flipperTitles(title);
+        }
         // Start lengthy operation in a background thread
         new Thread(new Runnable() {
             public void run() {
@@ -31,19 +46,40 @@ public class SplashActivity extends Activity {
             }
         }).start();
     }
-    String[] launcher_message = {"Initializing\tsystem","Starting\tengine","opening\tpayment\tgateway",
-            "initializing\tstack","Initializing\tsecurity","Loading\tpools","More\tsecurity\tinitialization",
-            "Starting\tsystem","Loading\tapp","Updating\tlocal\tdatabase"};
+
+    private void flipperImages(int image) {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        flipper.addView(imageView);
+        flipper.setFlipInterval(2000);
+        flipper.setAutoStart(true);
+        flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+    }
+    private void flipperTitles(int title){
+        TextView desc = new TextView(this);
+        desc.setTextColor(getResources().getColor(R.color.colorWhite));
+        desc.setTextSize(18);
+        desc.setText(title);
+        textFlipper.addView(desc);
+        textFlipper.setFlipInterval(2000);
+        textFlipper.setAutoStart(true);
+        textFlipper.setInAnimation(this,android.R.anim.slide_in_left);
+        textFlipper.setOutAnimation(this,android.R.anim.slide_out_right);
+    }
+    String[] launcher_message = {"Initializing system", "Starting engine", "opening payment gateway",
+            "initializing stack", "Initializing security", "Loading pools", "More security initialization",
+            "Starting system", "Loading app", "Updating local database"};
 
     private void doWork() {
-        for (int progress=0,i=0; progress<101; progress+=10,i++) {
+        for (int progress = 0; progress < launcher_message.length; progress ++) {
             try {
-                Thread.sleep(1000);
-                mProgress.setProgress(progress);
-                    loaderLabel.setText(launcher_message[i]+" "+progress+" %");
+                Thread.sleep(5000);
+                //mProgress.setProgress(progress);
+                loaderLabel.setText(launcher_message[progress] + " " + (progress+1)*10 + " %");
             } catch (Exception e) {
                 e.printStackTrace();
-                           }
+            }
         }
     }
 
