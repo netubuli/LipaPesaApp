1f02eb02f3212929d53d53e1748274bc8b7f6f5a
TheOnlySmartBoy <o2jose43@gmail.com>
2020-01-07 18:39:58 +0300

    Uploaded new logo

--- app/src/main/java/com/otemainc/mlipa/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/otemainc/mlipa/ui/gallery/GalleryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,15 +13,44 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.otemainc.mlipa.R;
+import com.otemainc.mlipa.util.AppConfig;
+import com.otemainc.mlipa.util.adapter.TransactionHistoryAdapter;
+import com.otemainc.mlipa.util.model.TransactionHistory;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    //a list to store all the transaction histories
+   private List<TransactionHistory> transactionHistories;
+
+    //the table layout
+    TableLayout tableLayout;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        //getting the recyclerview from xml
+        tableLayout = root.findViewById(R.id.transactionTableLayout);
+
+        //initializing the productlist
+        transactionHistories = new ArrayList<>();
+
+        //this method will fetch and parse json
+        //to display it in recyclerview
+        loadProducts();
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -29,5 +59,57 @@ public class GalleryFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    private void loadProducts() {
+        /*
+         * Creating a String Request
+         * The request type is GET defined by first parameter
+         * The URL is defined in the second parameter
+         * Then we have a Response Listener and a Error Listener
+         * In response listener we will get the JSON response as a String
+         * */
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, AppConfig.URL_TRANSAC_DETAILS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting the string to json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //traversing through all the object
+                            for (int i = 0; i < array.length(); i++) {
+
+                                //getting product object from json array
+                                JSONObject product = array.getJSONObject(i);
+
+                                //adding the product to product list
+                                transactionHistories.add(new TransactionHistory(
+                                        transactionHistories.getInt("id"),
+                                        transactionHistories.getString("code"),
+                                        transactionHistories.getString("tdate"),
+                                        transactionHistories.getString("sender"),
+                                        transactionHistories.getDouble("amount"),
+                                        transactionHistories.getString("receiver")
+                                ));
+                            }
+
+                            //creating adapter object and setting it to recyclerview
+                            TransactionHistoryAdapter adapter = new TransactionHistoryAdapter(getActivity().getApplicationContext(), transactionHistories);
+                            tableLayout.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        //adding our stringrequest to queue
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/otemainc/mlipa/util/adapter/TransactionHistoryAdapter.java
@@ -0,0 +1,58 @@
+package com.otemainc.mlipa.util.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.otemainc.mlipa.R;
+import com.otemainc.mlipa.util.model.TransactionHistory;
+
+import java.util.List;
+
+public class TransactionHistoryAdapter extends TableLayout.Adapter<TransactionHistoryAdapter.TransactionViewHolder>{
+
+    private Context mCtx;
+    private List<TransactionHistory> transactionHistories;
+
+    public TransactionHistoryAdapter(Context mCtx, List<TransactionHistory> transactionHistories) {
+        this.mCtx = mCtx;
+        this.transactionHistories = transactionHistories;
+    }
+    @Override
+    public TransactionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.transactionTableLayout, null);
+        return new TransactionViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(TransactionViewHolder holder, int position) {
+        TransactionHistory history = transactionHistories.get(position);
+
+        holder.textViewcode.setText(history.getCode());
+        holder.textViewTdate.setText(history.getTdate());
+        holder.textViewSender.setText(history.getSender());
+        holder.textViewAmount.setText(String.valueOf(history.getAmount()));
+        holder.textViewReceiver.setText(history.getReceiver());
+    }
+    @Override
+    public int getItemCount() {
+        return transactionHistories.size();
+    }
+    class TransactionViewHolder extends TableLayout.ViewHolder {
+
+        TextView textViewcode, textViewTdate, textViewSender, textViewAmount, textViewReceiver;
+
+        public TransactionViewHolder(View itemView) {
+            super(itemView);
+
+            textViewcode = itemView.findViewById(R.id.code);
+            textViewTdate = itemView.findViewById(R.id.tdate);
+            textViewSender = itemView.findViewById(R.id.secondary);
+            textViewAmount = itemView.findViewById(R.id.amount);
+            textViewReceiver = itemView.findViewById(R.id.receiver);
+        }
+    }
+}
