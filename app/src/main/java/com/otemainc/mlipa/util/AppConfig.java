30737f62fdf59a7c8da58370b33c5d74c84eba88
TheOnlySmartBoy <o2jose43@gmail.com>
2020-01-22 11:17:20 +0300

    Begun writting withdrawal logic

--- app/src/main/java/com/otemainc/mlipa/util/AppConfig.java
+++ app/src/main/java/com/otemainc/mlipa/util/AppConfig.java
@@ -7,6 +7,8 @@ public class AppConfig {
     public static String URL_REGISTER = "http://192.168.100.250:8082/mlipaapi/client/register.php";
     //Server send money url
     public static String URL_SEND = "http://192.168.100.250:8082/mlipaapi/account/transfer.php";
+    //Server send money url
+    public static String URL_WITHDRAW = "http://192.168.100.250:8082/mlipaapi/account/withdraw.php";
     //balance url
     public static String URL_BALANCE ="http://192.168.100.250:8082/mlipaapi/account/getbalance.php";
     //transaction details url
